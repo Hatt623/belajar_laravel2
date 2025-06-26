afc1b71d87338e21e846b2f84cac85dcd74b404f
Hatt623 <mrplanet623@gmail.com>
2025-06-26 17:25:10 +0700

    06/26/2025

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -135,9 +135,17 @@
                                                         </li>
             
                                                         <li>
-                                                            <a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Add To Cart" data-bs-original-title="Add To Cart">
+                                                            <a href="{{ route('cart.add', $data->id) }}" data-bs-toggle="tooltip" onclick="event.preventDefault(); document.getElementById('add-to-cart-form-{{ $data->id }}').submit();"
+                                                                data-bs-toggle="tooltip" title="Add To Cart">
                                                                 <i class="fas fa-shopping-cart"></i>
-                                                            </a>                                             
+                                                            </a>    
+                                                            
+                                                             <form id="add-to-cart-form-{{ $data->id }}"
+                                                                action="{{ route('cart.add', $data->id) }}" method="POST"
+                                                                class="d-none">
+                                                                @csrf
+                                                                <input type="hidden" name="qty" value="1">
+                                                            </form>
                                                         </li>
                                    
                                                     </ul>   
