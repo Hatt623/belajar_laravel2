6dbb6e9160c52067790bc7e2e20f61dab01bc5a6
Hatt623 <mrplanet623@gmail.com>
2025-06-30 15:46:48 +0700

    06/30/2025

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,8 +30,8 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
+                    <ul class="navbar-nav me-auto">    
+                        
                     </ul>
 
                     <!-- Right Side Of Navbar -->
